a27d88543aa9031a02570354740c12255a931d8d
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2020-09-04 15:19:47 +0300

    onBackPressed configured

--- app/src/main/java/com/example/gadslearderboard/ui/main/SubmissionActivity.java
+++ app/src/main/java/com/example/gadslearderboard/ui/main/SubmissionActivity.java
@@ -1,11 +1,15 @@
 package com.example.gadslearderboard.ui.main;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.gadslearderboard.R;
+
+import java.util.Objects;
 
 public class SubmissionActivity extends AppCompatActivity {
 
@@ -14,5 +18,19 @@ public class SubmissionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.submission_activity);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        getSupportActionBar(toolbar);
+toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        onBackPressed();
     }
+});
+
+    }
+
+    private void getSupportActionBar(Toolbar toolbar) {
+
+    }
+
 }
