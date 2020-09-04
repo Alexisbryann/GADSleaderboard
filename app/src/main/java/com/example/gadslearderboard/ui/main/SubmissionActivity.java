a351442bbf430eca53756312b4d5b2650f3e0e88
KIPROTICH BRIAN <bryannkoech7@gmail.com>
2020-09-04 15:58:53 +0300

    Changed textviews to plain text in submission_activity.xml

--- app/src/main/java/com/example/gadslearderboard/ui/main/SubmissionActivity.java
+++ app/src/main/java/com/example/gadslearderboard/ui/main/SubmissionActivity.java
@@ -2,6 +2,8 @@ package com.example.gadslearderboard.ui.main;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,28 +11,35 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.example.gadslearderboard.R;
 
-import java.util.Objects;
-
 public class SubmissionActivity extends AppCompatActivity {
+
+    private TextView mProjectSubmission;
+    private TextView mFirstName;
+    private TextView mSecondName;
+    private TextView mEmailAddress;
+    private TextView mGitHubLink;
+    private Button mSubmitActivity;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.submission_activity);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        getSupportActionBar(toolbar);
-toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+
+        mToolbar = findViewById(R.id.toolbar);
+        mProjectSubmission = findViewById(R.id.text_view_project_submission);
+        mFirstName = findViewById(R.id.editTextTextFirstName);
+        mSecondName = findViewById(R.id.editTextTextLastName);
+        mEmailAddress = findViewById(R.id.text_view_email_address);
+        mGitHubLink = findViewById(R.id.editTextTextGithubLink);
+        mSubmitActivity = findViewById(R.id.button_submit_activity);
+
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View view) {
         onBackPressed();
     }
 });
-
     }
-
-    private void getSupportActionBar(Toolbar toolbar) {
-
-    }
-
 }
